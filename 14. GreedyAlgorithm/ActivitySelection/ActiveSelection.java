65c751b4972b0dec8810bb2d485bde5e0c9718a2
Prajwal Jayant Somalkar <prajwalsomalkar025@gmail.com>
2023-04-21 14:13:54 +0530

    Adding ActiveSelection problem code

--- 14. GreedyAlgorithm/ActivitySelection/ActiveSelection.java
+++ 14. GreedyAlgorithm/ActivitySelection/ActiveSelection.java
@@ -1,5 +1,43 @@
 //You are given n activities with their start and end times.Select the maximum number of aactivities that can be performed by a single person, assuming that a person can only work on a single activity at a time. Activities are sorted according to end time.
 
+//Time Complexity : O(nlogn)
+import java.util.*;
+
 public class ActiveSelection {
-    
+    public static void main(String args[]) {
+        int start[] = { 1, 3, 0, 5, 8, 5 };
+        int end[] = { 2, 4, 6, 7, 9, 9 };
+
+        // Sorting --> if end time is not sorted then we have to first sort it
+        int activities[][] = new int[start.length][3];
+        for (int i = 0; i < start.length; i++) {
+            activities[i][0] = i; // index
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+        // lambda function -> shortform
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+
+        // end time basis sorted
+        int maxAct = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        // 1st activity
+        maxAct = 1;
+        ans.add(activities[0][0]);
+        int lastEnd = activities[0][2];
+        for (int i = 1; i < end.length; i++) {
+            if (activities[i][1] >= lastEnd) {
+                // activity select
+                maxAct++;
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
+            }
+        }
+        System.out.println("max Activities = " + maxAct);
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print("A" + ans.get(i) + " ");
+        }
+        System.out.println();
+    }
 }
