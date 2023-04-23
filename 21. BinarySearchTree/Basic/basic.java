7769bcc130e06bca3e3d80a3c1a44441ca01fd50
Prajwal Jayant Somalkar <prajwalsomalkar025@gmail.com>
2023-04-23 14:18:22 +0530

    Adding Mirro a BST code

--- 21. BinarySearchTree/Basic/basic.java
+++ 21. BinarySearchTree/Basic/basic.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class basic {
 
     static class Node {
@@ -90,8 +92,59 @@ public class basic {
         return root;
     }
 
+    // print in range
+    public static void printInRange(Node root, int k1, int k2) {
+        if (root == null) {
+            return;
+        }
+        if (root.data >= k1 && root.data <= k2) {
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data + " ");
+            printInRange(root.right, k1, k2);
+        } else if (root.data < k1) {
+            printInRange(root.right, k1, k2);
+        } else {
+            printInRange(root.left, k1, k2);
+        }
+    }
+
+    // Root to leaf Path -> Problem
+    public static void printPath(ArrayList<Integer> path) {
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i) + "-> ");
+        }
+        System.out.println("Null");
+    }
+
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.data);
+        if (root.left == null && root.right == null) {
+            printPath(path);
+        }
+        printRoot2Leaf(root.left, path);
+        printRoot2Leaf(root.right, path);
+        path.remove(path.size() - 1);
+    }
+
+    // valid BST
+    public static boolean isValidBST(Node root, Node min, Node max) {
+        if (root == null) {
+            return true;
+        }
+        if (min != null && root.data <= min.data) {
+            return false;
+        } else if (max != null && root.data >= max.data) {
+            return false;
+        }
+
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
+
     public static void main(String args[]) {
-        int values[] = { 5, 1, 3, 4, 2, 7 };
+        int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
         Node root = null;
         for (int i = 0; i < values.length; i++) {
             root = insert(root, values[i]);
@@ -104,9 +157,13 @@ public class basic {
             System.out.println("Not found");
         }
 
-        root = delete(root, 1);
-        System.out.println();
+        ArrayList<Integer> path = new ArrayList<>();
+        printRoot2Leaf(root, path);
 
-        inorder(root);
+        if (isValidBST(root, null, null)) {
+            System.out.println("It is a valid BST");
+        } else {
+            System.out.println("Not valid");
+        }
     }
 }
