c7255f8ca2e326f4a3b39fa6d15164dd5a0c90e3
Prajwal Jayant Somalkar <prajwalsomalkar025@gmail.com>
2023-06-27 21:48:57 +0530

    adding Disjoint set union, FloodFill Algorithm and Kruskals Algorith

---
+++ 20. Graph/FloodFill_Algorithm/FloodFill_Algorithm.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class FloodFill_Algorithm {
+
+    // O(m*n)
+    public int [][] helper(int [][] image,int sr,int sc,int color,boolean vis[][],int orgCol){
+
+        if(sr<0 || sc<0 || sr >= image.length || sc>= image[0].length || vis[sr][sc] || image[sr][sc] != orgCol){
+            return;
+        }
+        image[sr][sc] = color;
+        //left
+        helper(image, sr, sc-1, color, vis, orgCol)
+        //right
+        helper(image, sr, sc+1, color, vis, orgCol)
+        //up
+        helper(image, sr-1, sc, color, vis, orgCol)
+        //down
+        helper(image, sr+1, sc, color, vis, orgCol)
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        boolean vis[][] = new boolean[image.length][image[0].length];
+        helper(image, sr, sc, color, vis, image[sr][sc]);
+        return image;
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
