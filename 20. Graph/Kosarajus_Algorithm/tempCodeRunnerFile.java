11cd3becd4ce4c64ac4cfb2a1cc0b830d46df523
Prajwal Jayant Somalkar <prajwalsomalkar025@gmail.com>
2023-06-29 19:22:26 +0530

    Adding Kosarajus Algorithm

---
+++ 20. Graph/Kosarajus_Algorithm/tempCodeRunnerFile.java
@@ -0,0 +1,11 @@
+public static void topSort(ArrayList<Edge> graph[], int curr, boolean vis[], Stack<Integer> s) {
+        vis[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!vis[e.dest]) {
+                topSort(graph, e.dest, vis, s); // Use e.dest as the source vertex
+            }
+        }
+        s.push(curr);
+    }
